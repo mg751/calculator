a42e6b1327c3f614286946d0e2fdde9ed64e4a6e
mg751 <mgoreansky@gmail.com>
2022-09-24 13:39:36 +0300

    Add working display and clear btn

--- script.js
+++ script.js
@@ -50,8 +50,39 @@ let operate = function (num0, operator, num1){
 }};
 
 
+// 5. Make the calculator work! You’ll need to store the first number that is 
+//input into the calculator when a user presses an operator, 
+// and also save which operation has been chosen and then operate() 
+// on them when the user presses the “=” key.
+// You should already have the code that can populate the display, 
+//so once operate() has been called, update the display with the ‘solution’ 
+//to the operation.
+//This is the hardest part of the project. You need to figure out how to store 
+// all the values and call the operate function with them.
+
+const calcDisplay = document.getElementById("displayvalue");
+const numBtns = document.getElementById("numbers").children;
+const clearBtn = document.getElementById("clearbtn");
+const multiplyBtn = document.getElementById("multiplybtn");
+const addBtn = document.getElementById("addbtn");
+const divideBtn = document.getElementById("dividebtn");
+const subtractBtn  = document.getElementById("subtractbtn");
+const equalsBtn  = document.getElementById("equalsbtn");
 
 
+function storeInput(){
+    console.log(this.textContent);
+    calcDisplay.textContent += this.textContent;
+};
+
+function clearInput(){
+    calcDisplay.textContent = "";
+} 
 
 
+for (let i = 0; i < numBtns.length; i++) {
+    numBtns[i].addEventListener("click", storeInput);
+}
+
+clearBtn.addEventListener("click", clearInput);
 
